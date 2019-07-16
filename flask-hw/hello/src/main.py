7a506f6354f2dc94da06d109b5fe3431ee7369cc
Richard Chow <rchow@mitns.com>
2019-07-16 10:04:04 -0700

    hello world - passed dictionary to template

--- flask-hw/hello/src/main.py
+++ flask-hw/hello/src/main.py
@@ -3,8 +3,11 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def helloIndex():
-  return render_template('index.html')
+def index():
+    name = "Rich Chow"
+    age = "21"
+    context = { 'a': "Rich", 'b':"Chow", 'c':"M" }
+    return render_template("hello.html", name=name, age=age, **context)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True, port=80)
+    app.run(host="0.0.0.0", debug=True, port=5000)
